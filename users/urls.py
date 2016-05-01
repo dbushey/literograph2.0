6c205e3c57715570b198005a0a5f8564362a2572
Daiane Bushey <daianebushey@Daianes-MacBook.local>
2016-05-01 00:25:58 -0400

    Made the landing page and linked to sign up / login

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^login/$', 'django.contrib.auth.views.login', name='signin'),
 	url(r'^logout/$', 'django.contrib.auth.views.logout'),
     url(r'^signup/$', views.register, name='register'),
 
